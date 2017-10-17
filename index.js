f884cc603496d3727c789c77b5fd0221a613dd16
shixq <shixq3@gmail.com>
2017-10-17 17:57:54 +0800

    添加中英文分词字段

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ var P2PSpider = require('./lib');
 var db = require('./db/mysql');
 var torrent = require('./db/torrent');
 var moment = require('moment');
+var Segment = require('segment');
 
 var p2p = P2PSpider({
     nodesMaxSize: 400,
     maxConnections: 800,
     timeout: 10000
 });
+
+// 创建实例
+var segment = new Segment();
+// 使用默认的识别模块及字典，载入字典文件需要1秒，仅初始化时执行一次即可
+segment.useDefault();
 
 p2p.ignore(function (infohash, rinfo, callback) {
     var torrentFilePathSaveTo = path.join(__dirname, "torrents", infohash + ".torrent");
@@ -27,26 +33,32 @@ p2p.on('metadata', function (metadata) {
     var time = moment().format('YYYY-MM-DD');
     var torrentDir = path.join(__dirname, "torrents/" + time);
     var torrentFilePathSaveTo = path.join(torrentDir, metadata.infohash + ".torrent");
-    var sql = "insert into t_torrent(id,name,type,size,files,create_date,enable,magnet,torrent_path) values (?,?,?,?,?,?,?,?,?)";
+    var sql = "insert into t_torrent(id,name,name_separate,type,size,files,create_date,enable,magnet,torrent_path) values (?,?,?,?,?,?,?,?,?,?)";
     var data = torrent(metadata.info);
     var now = moment().format('YYYY-MM-DD HH:mm:ss');
     var magnet = 'magnet:?xt=urn:btih:' + metadata.infohash;
     var files = JSON.stringify(data.files);
     files = files.replace(/,/g, "\,");
     var torrentPath = "/torrents/" + time + "/" + metadata.infohash + ".torrent";
-    var sqlParam = [metadata.infohash, data.name, data.type, data.size, files, now, 1, magnet, torrentPath];
+
+    var _fenci = segment.doSegment(data.name, {
+        simple: true,
+        stripPunctuation: true
+    });
+    
+    var sqlParam = [metadata.infohash, data.name, _fenci.join(' '), data.type, data.size, files, now, 1, magnet, torrentPath];
     db.insert(sql, sqlParam, function (err, result) {
         if (err) {
             console.error(err);
         } else {
             console.log("insert torrent result:" + result);
 
-            var sqlInsert = "insert into t_bt(torrent_id,content,create_date) values (?,?,?)";
-            db.insert(sqlInsert, [metadata.infohash, bencode.encode({ 'info': metadata.info }), time], function (err, result) {
-                if (err) {
-                    console.error(err);
-                }
-            });
+            // var sqlInsert = "insert into t_bt(torrent_id,content,create_date) values (?,?,?)";
+            // db.insert(sqlInsert, [metadata.infohash, bencode.encode({ 'info': metadata.info }), time], function (err, result) {
+            //     if (err) {
+            //         console.error(err);
+            //     }
+            // });
 
             // fs.exists(torrentDir,function(exist) {
             //     if(!exist) {
--- test1.js
+++ test1.js
@@ -1,31 +1,30 @@
 var db = require('./db/mysql');
-var moment = require('moment');
+var Segment = require('segment');
 
-db.query("select id,files from t_torrent",function(qerr,vals,fields) {
-    if(qerr) {
+db.query("select id,name from t_torrent", function (qerr, vals, fields) {
+    if (qerr) {
         return console.error(qerr);
     }
-    var i;
+    // 创建实例
+    var segment = new Segment();
+    // 使用默认的识别模块及字典，载入字典文件需要1秒，仅初始化时执行一次即可
+    segment.useDefault();
     var length = vals.length;
     console.log("length:" + length);
-    for(i = 0;i<length;i++) {
-        console.log("update:" + i);
-        var sql = "insert into t_files(name,size,torrent_id) values (?,?,?)";
-        var files = JSON.parse(vals[i].files);
-        var j;
-        var id = vals[i].id;
-        if(files) {
-            for(j = 0;j<files.length;j++) {
-                // console.log(files[j].name);
-                var name = files[j].name;
-                var size = files[j].size;
-                db.insert(sql,[name,size,id],function(err,result) {
-                    if(err) {
-                        console.error(err);
-                    }
-                });
+    for (var i = 0; i < length; i++) {
+        var _fenci = segment.doSegment(vals[i].name, {
+            simple: true,
+            stripPunctuation: true
+        });
+        // console.log(vals[i].name);
+        // console.log(_fenci.join(' '));
+        var sql = "update t_torrent set name_separate = ? where id = ?";
+        db.update(sql, [_fenci.join(' '), vals[i].id], function (err, result) {
+            if (err) {
+                return console.error(err);
             }
-        }
+            console.log("update " + i + " success");
+        });
     }
-    
+
 });
